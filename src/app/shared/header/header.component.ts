313d74a3609d4658e9b30134c4d1eb0677d90716
Wagner Esser <waesser@gmail.com>
2018-11-27 19:24:40 -0200

    #33 - Estilização do cabeçalho com menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
+  bumblebee = '../../assets/bumblebee.png';
+  logo = '../../assets/logo-4.png';
 
   constructor() { }
 
