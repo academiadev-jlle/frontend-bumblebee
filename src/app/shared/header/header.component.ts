930f160dc2fa45db4c0283782b7eea383d649cb7
Wagner Esser <waesser@gmail.com>
2018-12-03 19:32:07 -0200

    #102 - Auto contraste

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -10,10 +10,16 @@ export class HeaderComponent implements OnInit {
   bumblebee = '../../assets/bumblebee.png';
   logo = '../../assets/logo-4.png';
   isCollapsed = true;
+  body: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  autoContraste() {
+    this.body = document.getElementById('body');
+    this.body.className = this.body.className === '' ? 'autoContraste' : '';
   }
 
 }
