b968b58246fe892727c0a93197d9cd78e0cfdf24
Wagner Esser <waesser@gmail.com>
2018-12-04 01:29:43 -0200

    ajustes

--- src/app/public/slider/slider.component.ts
+++ src/app/public/slider/slider.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
+  // tslint:disable-next-line:max-line-length
+  slider1 = '../../../assets/slider-1.jpg';
+  slider2 = '../../../assets/slider-2.jpg';
+  slider3 = '../../../assets/slider-3.jpg';
+
   constructor() { }
 
   ngOnInit() {
