a9a553846a4d3d38a60c61936ca8d17dca91d3b1
Victor Mafra <vlmmafra@gmail.com>
2018-12-07 06:17:11 -0200

    #103 - rxjs básico no cadastro de usuário

--- src/app/public/user-cad/user-cad.component.ts
+++ src/app/public/user-cad/user-cad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-cad',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCadComponent implements OnInit {
 
-  constructor() { }
+  cadastroForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.cadastroForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      nome: ['', Validators.required],
+      telefone: ['', Validators.required],
+      senha: ['', Validators.required],
+      confirmacaoSenha: ['', Validators.required],
+    });
   }
+
 
 }
